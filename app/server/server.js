ccba60ea7b0350a63c15fc1008bfc10e1f3f6b23
samy <samy_leni@hotmail.fr>
2019-11-05 09:40:10 +0100

    feat: node init

---
+++ app/server/server.js
@@ -0,0 +1,12 @@
+require('dotenv').config({
+    path: __dirname + '/../../.env'
+});
+const serverPort = process.env.SERVERPORT;
+
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+
+app.use(express.static(__dirname + '/../client'));
+
+http.listen(serverPort, () => console.log('\x1b[33m%s\x1b[0m', "Server is listening on port " + serverPort));
