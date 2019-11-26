df887a3a7916c0d8d93b83843d288e68e16e220d
samy <samy_leni@hotmail.fr>
2019-11-26 09:57:38 +0100

    upd: keyboard listener

--- app/client/js/keyboardListener.js
+++ app/client/js/keyboardListener.js
@@ -1,5 +1,7 @@
 class KeyboardListener {
     constructor() {
+        this.keys = {};
+
         this.keyCodes = new Map([
             [81, "left"],
             [90, "up"],
@@ -9,21 +11,14 @@ class KeyboardListener {
             [80, "b"]
         ]);
 
-        this.listen = socket => {
-            var pressed = new Map();
-            this.keyCodes.forEach(code => pressed.set(code, false));
-            var handler = event => {
-                if (this.keyCodes.get(event.keyCode) !== undefined) {
-                    var down = event.type === "keydown";
-                    pressed.set(this.keyCodes.get(event.keyCode), down);
-                    event.preventDefault();
-                }
-                var inputs = {};
-                pressed.forEach((value, key) => inputs[key] = value);
-                socket.emit('inputs', inputs);
-            };
-            addEventListener("keydown", handler);
-            addEventListener("keyup", handler);
-        };
+        this.handler = event => {
+            if (this.keyCodes.has(event.keyCode)) {
+                this.keys[this.keyCodes.get(event.keyCode)] = event.type === "keydown";
+                event.preventDefault();
+            }
+        }
+
+        addEventListener("keydown", this.handler);
+        addEventListener("keyup", this.handler);
     }
 }
